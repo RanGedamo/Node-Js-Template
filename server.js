07521e9db92c98998e09621136d59946a6b28d6e
RanGedamo <mcrangedamo@gmail.com>
2022-11-13 14:16:04 +0200

    server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({extended:true}));
 app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
